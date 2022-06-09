7e2e41d67f4be928524f82964bf3b200492ee2f9
IzudinRojak <107164944+IzudinRojak@users.noreply.github.com>
2022-06-09 12:15:22 +0700

    Update L200200251_Karmila Muhammad Izudin Rojak_quiz asd kelas I.py

--- L200200251_Karmila Muhammad Izudin Rojak_quiz asd kelas I.py
+++ L200200251_Karmila Muhammad Izudin Rojak_quiz asd kelas I.py
@@ -1,13 +1,22 @@
 import re
 #nomor 1
 
-pola  = r'@[A-Za-z]+[0-9.\_]+'
+pola1 = r'@[A-Za-z]+[0-9.\_]+'
+pola2 = r'@[A-Za-z]+[0-9]+.\_'
+pola3 = r'@[A-Za-z]+.\_'
 uname = input('Masukkan Username: ')
-cocok = re.findall(pola, uname)
-if cocok:
-    if uname == cocok[0]:
+cocok1 = re.findall(pola1, uname)
+cocok2 = re.findall(pola2, uname)
+cocok3 = re.findall(pola3, uname)
+if cocok3:
+    if uname == cocok3[0]:
+        print('FAILED')
+elif cocok2:
+    print('PASS')
+elif cocok1:
+    if uname == cocok1[0]:
         print('PASS')
-    elif uname != cocok[0]:
+    elif uname != cocok1[0]:
         print('FAILED')
 else:
     print('FAILED')
