750fb8a469810d599a2d4042829fe8cd13a1dc08
IzudinRojak <107164944+IzudinRojak@users.noreply.github.com>
2022-06-21 15:10:04 +0700

    Update L200200251_Karmila Muhammad Izudin Rojak_quiz asd kelas I.py

--- L200200251_Karmila Muhammad Izudin Rojak_quiz asd kelas I.py
+++ L200200251_Karmila Muhammad Izudin Rojak_quiz asd kelas I.py
@@ -1,26 +1,15 @@
 import re
-#nomor 1
-
-pola1 = r'@[A-Za-z]+[0-9.\_]+'
-pola2 = r'@[A-Za-z]+[0-9]+.\_'
-pola3 = r'@[A-Za-z]+.\_'
+#nomor1
+    
 uname = input('Masukkan Username: ')
-cocok1 = re.findall(pola1, uname)
-cocok2 = re.findall(pola2, uname)
-cocok3 = re.findall(pola3, uname)
-if cocok3:
-    if uname == cocok3[0]:
-        print('FAILED')
-elif cocok2:
-    print('PASS')
-elif cocok1:
-    if uname == cocok1[0]:
-        print('PASS')
-    elif uname != cocok1[0]:
-        print('FAILED')
-else:
+pola = r'@[\w]+[\d]+\_*'
+cocok = re.findall(pola,uname)
+if not cocok:
     print('FAILED')
-
+elif cocok[0] != uname:
+    print('FAILED')
+else:
+    print('PASS')
 
 #nomor 2
 a = open('teks.txt','r')
